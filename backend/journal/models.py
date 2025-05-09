88c1c323d758bb61ff49d32b8691d61317d8eaf6
Kyle <imkyle1988@gmail.com>
2025-05-09 11:31:27 -0400

    Added JournalEntry model and had successful test entry appear in DB

--- backend/journal/models.py
+++ backend/journal/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class JournalEntry(models.Model):
+    date = models.CharField(max_length=10)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Entry for {self.date}: {self.content[:50]}"
